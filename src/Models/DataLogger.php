68851c46d96e4ce66c17b720d1321c3739f575a5
JC <jc@insowe.com>
2020-04-10 01:46:50 +0800

    log deleted data (SoftDelete)

--- src/Models/DataLogger.php
+++ src/Models/DataLogger.php
@@ -13,20 +13,33 @@ trait DataLogger
     
     public static function getDataLogType()
     {
-        $className = substr(strrchr(__CLASS__, "\\"), 1); 
+        $className = substr(strrchr(__CLASS__, "\\"), 1);
         return Str::kebab($className);
     }
     
     public function toLogFile()
     {
-        $item = self::find($this->id);
-        $item->setHidden([
-            'updated_at',
-            'deleted_at',
-            'log_id',
-        ]);
+        if (in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this))) {
+            $item = self::withTrashed()->find($this->id);
+        }
+        else {
+            $item = self::find($this->id);
+        }
         
-        return $item->toJson();
+        if (is_null($item))
+        {
+            return $this->toJson();
+        }
+        else
+        {
+            $item->setHidden([
+                'updated_at',
+                'deleted_at',
+                'log_id',
+            ]);
+
+            return $item->toJson();
+        }
     }
     
     public function setLogId($logId)
